e34e2d543e8ed58bc66d306b1b9ed11bf37680d2
David Cacuango <cacuangodarwin1999@gmail.com>
2022-10-28 09:00:37 -0500

    pagina 404

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Formulario from "./Formulario";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-<Formulario/>
+<PageNotFound/>
   );
 }
 
